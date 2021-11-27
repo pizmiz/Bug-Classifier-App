6fe273b3b904d431835300e311996d0621c8d854
Ethan Britton <brittoea@mail.uc.edu>
2021-11-27 18:19:52 -0500

    Added Uploader Page and Analysis Page

--- Homepage.py
+++ Homepage.py
@@ -9,23 +9,31 @@ Upload_Path = os.path.dirname(os.path.realpath(__file__)) + "\\File_Upload"
 
 if os.path.exists(Upload_Path):
     shutil.rmtree(Upload_Path)
-os.makedirs(Upload_Path)   
+os.makedirs(Upload_Path)
 
 app.config['UPLOAD_FOLDER'] = Upload_Path
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/uploader", methods=['GET', 'POST'])
-def Upload():
+def upload():
     if os.path.exists(Upload_Path):
         shutil.rmtree(Upload_Path)
-        os.makedirs(Upload_Path) 
+        os.makedirs(Upload_Path)
     if request.method == 'POST':
         f = request.files['file']
-        f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
-        return redirect(url_for("home"))
+        f.save(os.path.join(
+            app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+        return render_template("uploader.html", content=f.filename)
+
+
+@app.route("/analysis")
+def analysis():
+    return render_template("analysis.html")
 
 
 @app.route("/<anything>")
