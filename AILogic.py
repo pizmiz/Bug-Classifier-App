a33c99a8d474dfa3209491448ff1099da1a59c97
Ethan Britton <brittoea@mail.uc.edu>
2021-11-28 15:57:42 -0500

    Finished App

--- AILogic.py
+++ AILogic.py
@@ -1,43 +1,54 @@
+import tensorflow as tf
 import os
+import shutil
 from stat import FILE_ATTRIBUTE_SPARSE_FILE
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-import tensorflow as tf
-from tensorflow.keras import layers
-from tensorflow.keras import losses
-
-ModelDirectory = os.path.dirname(os.path.realpath(__file__)) + "\\AI\\FuncModel\\FuncModel"
-
-max_features = 10000
-sequence_length = 250
-
-vectorize_layer = layers.TextVectorization(
-    standardize='lower_and_strip_punctuation',
-    max_tokens=max_features,
-    output_mode='int',
-    output_sequence_length=sequence_length
-)
 
 
-model = tf.keras.models.load_model(ModelDirectory, compile=True)
+def PreformAnalysis(TestPath):
+    ModelDirectory = os.path.dirname(os.path.realpath(
+        __file__)) + "\\AI\\FuncModel\\FuncModel"
 
-#export_model = tf.keras.Sequential([
-#    vectorize_layer,
-#    model,
-#    layers.Activation('sigmoid')
-#])
+    model = tf.keras.models.load_model(ModelDirectory, compile=True)
 
-#export_model.compile(
-#    loss=losses.BinaryCrossentropy(from_logits=True), optimizer="adam", metrics=['accuracy']
-#)
+    Files = []
+    FilesEnds = os.listdir(TestPath)
+    for FileEnd in FilesEnds:
+        FileToAppend = TestPath + "\\" + FileEnd
+        Files.append(FileToAppend)
 
-TestStr = [
-    'Who are we anymore',
-    'This is broken send help',
-    'Functional',
-    'This component is not working correctly',
-    'Test',
-    'This is a non functional component'
-]
+    TestSet = []
 
+    for file in Files:
+        f = open(file, "r")
+        FileText = f.read()
+        TestSet.append(FileText)
+        f.close()
 
-print (model.predict(TestStr))
+    results = model.predict(TestSet)
+    print(results)
+
+    ResultsPath = os.path.dirname(os.path.realpath(__file__)) + "\\Results"
+    ResultsPathFunc = ResultsPath + "\\Functional"
+    ResultsPathNonFunc = ResultsPath + "\\Non-Functional"
+    ResultsPathUnknown = ResultsPath + "\\Unknown"
+
+    if os.path.exists(ResultsPath):
+        shutil.rmtree(ResultsPath)
+        os.makedirs(ResultsPath)
+    else:
+        os.makedirs(ResultsPath)
+
+    os.makedirs(ResultsPathFunc)
+    os.makedirs(ResultsPathNonFunc)
+    os.makedirs(ResultsPathUnknown)
+
+    MyIndex = 0
+    for file in Files:
+        if (float(results[MyIndex]) < .05):
+            shutil.copy(file, ResultsPathFunc)
+        elif (float(results[MyIndex]) > .95):
+            shutil.copy(file, ResultsPathNonFunc)
+        else:
+            shutil.copy(file, ResultsPathUnknown)
+        MyIndex = MyIndex + 1
--- Homepage.py
+++ Homepage.py
@@ -1,11 +1,18 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, send_file, request
 from werkzeug.utils import secure_filename
 import os
 import shutil
+import zipfile
+import AILogic
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 app = Flask(__name__)
 
 Upload_Path = os.path.dirname(os.path.realpath(__file__)) + "\\File_Upload"
+Test_Path = os.path.dirname(os.path.realpath(__file__)) + "\\TestSet"
+ResultsPath = os.path.dirname(os.path.realpath(__file__)) + "\\Results"
+ResultsZipPath = os.path.dirname(os.path.realpath(__file__)) + "\\Results.zip"
+FileName = ""
 
 if os.path.exists(Upload_Path):
     shutil.rmtree(Upload_Path)
@@ -26,14 +33,33 @@ def upload():
         os.makedirs(Upload_Path)
     if request.method == 'POST':
         f = request.files['file']
-        f.save(os.path.join(
-            app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
-        return render_template("uploader.html", content=f.filename)
+        if (f.filename.endswith('.zip')):
+            global FileName
+            FileName = f.filename
+            f.save(os.path.join(
+                app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+            return render_template("uploader.html", content=f.filename)
+        else:
+            return render_template("uploaderError.html")
 
 
 @app.route("/analysis")
 def analysis():
+    if os.path.exists(Test_Path):
+        shutil.rmtree(Test_Path)
+        os.makedirs(Test_Path)
+    with zipfile.ZipFile(Upload_Path + "\\" + FileName, 'r') as zip_ref:
+        zip_ref.extractall(Test_Path)
+    PassedTestPath = Test_Path + "\\" + FileName
+    PassedTestPath = PassedTestPath[:len(PassedTestPath) - 4]
+    AILogic.PreformAnalysis(PassedTestPath)
     return render_template("analysis.html")
+
+
+@app.route("/download")
+def download():
+    shutil.make_archive("Results", 'zip', ResultsPath)
+    return send_file(ResultsZipPath, as_attachment=True)
 
 
 @app.route("/<anything>")
